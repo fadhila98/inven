8db36d7f191dd5d6e006a64e5c0c1a989afd87f0
Your name <fadhilanasir@gmail.com>
2021-09-26 22:59:00 +0300

    run git reset --hard HEAD^

--- inven/productmovement/forms.py
+++ inven/productmovement/forms.py
@@ -5,9 +5,6 @@ from products.models import Product
 
 
 class ProductMovementForm(forms.ModelForm):
-    # def product_deets(request,self,*args, **kwargs):
-    #     product_name = Product.objects.get(kwargs={'title':Product.title})
-    #     self.save()
     
     from_location_qty = Product.objects.count()
     class Meta:
@@ -16,6 +13,5 @@ class ProductMovementForm(forms.ModelForm):
             'from_location',
             'to_location',
             'product_id',
-            'qty',
             
         ]
--- inven/productmovement/models.py
+++ inven/productmovement/models.py
@@ -4,6 +4,7 @@ from products.models import Product
 from django.db.models.deletion import CASCADE
 from django.urls import reverse
 
+
 # Create your models here.
 class ProductMovement(models.Model):
     # movement_id = models.
@@ -11,9 +12,22 @@ class ProductMovement(models.Model):
     product_id = models.ForeignKey(Product, on_delete=CASCADE)
     timestamp = models.DateTimeField(auto_now=True)
     to_location = models.ForeignKey(Location, related_name='location-to+',on_delete=models.CASCADE, default=True,blank= True)
-    qty = models.IntegerField()
+    # qty = models.IntegerField(null=True)
+    # qty_to_location = models.IntegerField()
     # product_name= models.ForeignKey(Product,on_delete=CASCADE,primary_key=True)
     
+    # @property
+    # def get_quantity(self):
+    #      if self.from_location== '' and self.to_location != '':
+    #         return self.qty_to_location == Product.objects.count
+    #     #  elif self.to_location== '' and self.from_location != '':
+    #     #     return self.qty_from_location ==+ 1
+
+    
+    # def save(self, *args, **kwargs):
+    #       self.qty_from_location = self.get_quantity
+    #       self.qty_to_location = self.get_quantity
+    #       super(ProductMovement, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.from_location
